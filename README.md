a2ec92077e3b22c9c56358d3d69da06a29228f71
unknown <mostafaomeromer@gmail.com>
2024-11-19 14:09:28 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MultiVendorLMS
+# LMS
--- UI/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ UI/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aedd1b4d998de9dd52ee520f34252c57b5516a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
